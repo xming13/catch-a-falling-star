dffa48b410164969934ace12c1f204a34930ed00
xming13 <xiaoming33@gmail.com>
2014-09-28 14:03:59 +0800

    Make xming-games css and js minified and reusable

---
+++ js/xming-games.js
@@ -0,0 +1,5 @@
+var xming_preload_image = new Image();
+xming_preload_image.src = "https://xming13.github.io/catch-a-falling-star/images/icons/icon-easter-bunnies.png";
+xming_preload_image.src = "https://xming13.github.io/catch-a-falling-star/images/icons/icon-easter-bunnies-hover.png";
+xming_preload_image.src = "https://xming13.github.io/catch-a-falling-star/images/icons/icon-catch-a-falling-star.png";
+xming_preload_image.src = "https://xming13.github.io/catch-a-falling-star/images/icons/icon-catch-a-falling-star-hover.png";
